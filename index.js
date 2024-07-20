37a16d2e6822e5b7cb20d0d9c25e850f5c48b2fe
valentin-orrit <vorrit@gmail.com>
2024-07-20 15:34:47 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { program } from "commander";
+
+program
+  .version("1.0.0")
+  .description("My Node CLI")
+  .option("-n, --name <type>", "Add your name")
+  .action((options) => {
+    console.log(`Hey, ${options.name}!`);
+  });
+
+program.parse(process.argv);
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Live project manager CLI
+This is a CLI app that uncompress .als files, extract the infos and put them in a json db.
+
+## Install
++ Clone the repo
++ [Install node](https://nodejs.org/en/download/package-manager)
++ initialize node
+```
+npm init
+```
