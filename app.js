935779731b53b8ee9b72fd15cb022b2a44ad13ed
valentin-orrit <vorrit@gmail.com>
2024-07-25 12:24:16 +0200

    promisified the functions in /lib

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 import { uncompress } from './lib/uncompress.js'
 import { parse } from './lib/parse.js'
+import chalk from 'chalk'
 
 
-uncompress()
-parse()
+async function main() {
+    try {
+        await uncompress()
+        console.log(chalk.green('Uncompression Completed. Parsing...'))
+        await parse()
+        console.log(chalk.green('Process Parsing Completed.'))
+    } catch (error) {
+        console.error(chalk.red('An error occurred:'), error)
+    }
+}
+
+main()
